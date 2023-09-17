578aacff14e5bd3492ca9b89c10543e137572ac5
han16935 <han1693516@gmail.com>
2023-09-17 19:09:00 +0900

    Make project and works successfully

---
+++ src/test/java/com/toyproject/toyBulletin/ToyBulletinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.toyBulletin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBulletinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
