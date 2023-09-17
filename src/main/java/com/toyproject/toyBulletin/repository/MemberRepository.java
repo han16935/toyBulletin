ef82747cd3740a5463d3fd521b763f3f3182544e
han16935 <han1693516@gmail.com>
2023-09-17 20:27:36 +0900

    Make MemberRepository interface and implement findByMemberId(@Param("memberId") String memberId)

--- src/main/java/com/toyproject/toyBulletin/repository/MemberRepository.java
+++ src/main/java/com/toyproject/toyBulletin/repository/MemberRepository.java
@@ -2,9 +2,17 @@ package com.toyproject.toyBulletin.repository;
 
 import com.toyproject.toyBulletin.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query(value = "SELECT PW FROM MEMBER WHERE MEMBER_ID = :memberId", nativeQuery = true)
+    String findByMemberId(@Param("memberId") String memberId);
 
+//    public boolean login(String memberId, String pw){
+//        if(findByMemberId(memberId).equals(pw)) return true;
+//        else return false;
+//    }
 }
