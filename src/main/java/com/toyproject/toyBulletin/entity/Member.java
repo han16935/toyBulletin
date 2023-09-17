5fddc279cf2ed4231c65a48b898e8ca0024c491e
han16935 <han1693516@gmail.com>
2023-09-17 19:30:11 +0900

    Make Member entity and insert data successfully in H2 DB

---
+++ src/main/java/com/toyproject/toyBulletin/entity/Member.java
@@ -0,0 +1,25 @@
+package com.toyproject.toyBulletin.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String memberId;
+
+    @Column(nullable = false)
+    private String pw;
+}
