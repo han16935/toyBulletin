370260b481c7b14357e79485364da37fcc19d6a5
han16935 <han1693516@gmail.com>
2023-09-19 19:07:44 +0900

    Make JoinService, add join() method

---
+++ src/main/java/com/toyproject/toyBulletin/service/JoinService.java
@@ -0,0 +1,28 @@
+package com.toyproject.toyBulletin.service;
+
+import com.toyproject.toyBulletin.dao.MemberDao;
+import com.toyproject.toyBulletin.entity.Member;
+import com.toyproject.toyBulletin.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class JoinService {
+
+    private MemberRepository memberRepository;
+    public Member join(MemberDao m) throws IllegalStateException{
+        // 1. MemberDao를 Member로 전환
+        Member willBeSaved = m.toMember();
+        // 1-1. 전환된 m 대해 nickName, memberId 중복 검사
+        //      검사 통과 불가 시, IllegalStateException
+
+        // 2. Member 저장
+
+        Member result = memberRepository.save(willBeSaved);
+        // 3. 1에서 만들어진 결과 반환
+        return result;
+    }
+}
