2a6b9f001b42feeb7c7540a6fa4ba0854c8cfc40
han16935 <han1693516@gmail.com>
2023-09-21 03:18:28 +0900

    Make ArticleDto in dto package

---
+++ src/main/java/com/toyproject/toyBulletin/dao/ArticleDto.java
@@ -0,0 +1,20 @@
+package com.toyproject.toyBulletin.dao;
+
+import com.toyproject.toyBulletin.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+public class ArticleDto {
+    private Long author;
+    private String title;
+    private String content;
+
+    public ArticleDto(Long author, String title, String content){
+        this.author = author;
+        this.title = title;
+        this.content = content;
+    }
+}
