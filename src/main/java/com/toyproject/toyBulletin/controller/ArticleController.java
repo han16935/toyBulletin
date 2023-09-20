3989daccbcc7f2b4e95d62a9d0dba7a7abde7d5e
han16935 <han1693516@gmail.com>
2023-09-20 23:39:07 +0900

    Add HttpServetResponse to getArticles(), to give client HTTP 401 when unauthorized

--- src/main/java/com/toyproject/toyBulletin/controller/ArticleController.java
+++ src/main/java/com/toyproject/toyBulletin/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.toyproject.toyBulletin.controller;
 
 import com.toyproject.toyBulletin.service.ArticleService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +15,12 @@ public class ArticleController {
     private ArticleService articleService;
 
     @GetMapping("/{id}/articles")
-    public String getArticles(@PathVariable Long id, Model model){
+    public String getArticles(@PathVariable Long id, Model model, HttpServletResponse response){
+        if(!articleService.isValid(id)) {
+            response.setHeader("Location", "http://localhost:8080");
+            response.setStatus(401); // HTTP 401 Found
+            return null; // No need to return a template name
+        }
         model.addAttribute("id", id);
         model.addAttribute("articles", articleService.getAllArticles());
         return "/articles";
