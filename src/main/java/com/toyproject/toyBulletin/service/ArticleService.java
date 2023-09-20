67a46eb9831a2d2f1b95a0de60ffb4d8c19ee54e
han16935 <han1693516@gmail.com>
2023-09-20 23:42:16 +0900

    Add isValid() method to find whether request is valid or not

--- src/main/java/com/toyproject/toyBulletin/service/ArticleService.java
+++ src/main/java/com/toyproject/toyBulletin/service/ArticleService.java
@@ -2,6 +2,7 @@ package com.toyproject.toyBulletin.service;
 
 import com.toyproject.toyBulletin.entity.Article;
 import com.toyproject.toyBulletin.repository.ArticleRepository;
+import com.toyproject.toyBulletin.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,15 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class ArticleService {
+    private MemberRepository memberRepository;
     private ArticleRepository articleRepository;
     public List<Article> getAllArticles() {
         return articleRepository.findAll();
+    }
+
+    public boolean isValid(Long id) {
+        if(memberRepository.findById(id).isPresent())
+          return memberRepository.findById(id).get().getIsValid();
+        else return false;
     }
 }
