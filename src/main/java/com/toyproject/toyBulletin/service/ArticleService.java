d76555b998c26a1d9e2eeb54fbab8568f26ac702
han16935 <han1693516@gmail.com>
2023-09-20 18:03:20 +0900

    Delete @Controller, @NoArgsConstructor in ArticleService.java and Add @Service

--- src/main/java/com/toyproject/toyBulletin/service/ArticleService.java
+++ src/main/java/com/toyproject/toyBulletin/service/ArticleService.java
@@ -3,14 +3,12 @@ package com.toyproject.toyBulletin.service;
 import com.toyproject.toyBulletin.entity.Article;
 import com.toyproject.toyBulletin.repository.ArticleRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Controller
+@Service
 @AllArgsConstructor
-@NoArgsConstructor
 public class ArticleService {
     private ArticleRepository articleRepository;
     public List<Article> getAllArticles() {
