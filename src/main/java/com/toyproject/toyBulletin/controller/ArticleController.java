fe309916822587ba5e033e378ae648556211e6ee
han16935 <han1693516@gmail.com>
2023-09-20 15:57:01 +0900

    Add getArticles(Model model) method (Article, ArticleService is not defined)

--- src/main/java/com/toyproject/toyBulletin/controller/ArticleController.java
+++ src/main/java/com/toyproject/toyBulletin/controller/ArticleController.java
@@ -1,4 +1,21 @@
 package com.toyproject.toyBulletin.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@AllArgsConstructor
 public class ArticleController {
+
+    private ArticleService articleService;
+
+    @GetMapping("/{id}/articles")
+    public String getArticles(@PathVariable Long id, Model model){
+        model.addAttribute("id", id);
+        model.addAttribute("articles", articleService.getAllArticles());
+        return "/articles";
+    }
 }
